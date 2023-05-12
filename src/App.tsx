55847eb64cb383c17e7c3cfcc7401e4747fe6c11
Athul Muralidharan <athulmurali@gmail.com>
2023-05-12 12:27:07 +0000

    fix it

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { RootFormComponent } from "./FormRenderer";
 export function App() {
-  return (
-    <div>
-      <RootFormComponent />
-    </div>
-  );
+  return <div id={container}>To be rendered here </div>;
+}
+
+const container = document.getElementById("container");
+if (container) {
+  container.innerHTML = RootFormComponent.render();
 }
