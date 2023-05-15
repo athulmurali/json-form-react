507ad539f38ba978e778a08ee3c73300302e0542
Athul Muralidharan <athul.muralidharan@oracle.com>
2023-05-15 04:49:21 -0700

    Added form into it

--- src/FormRenderer.tsx
+++ src/FormRenderer.tsx
@@ -1,68 +1,67 @@
 import React, { useState } from "react";
 import { FormStore } from "./FormStore";
 import { IFieldState, IFormState } from "./IStates";
-import { time } from "console";
 
 interface Props {
   forms?: FormStore;
   path: string[];
   formName?: string;
-  form: IFormState |IFieldState;
+  form: IFormState | IFieldState;
 }
 
 export function FormComponent(props: Props) {
   const [form, setForm] = useState(props.forms.getForm(props.formName));
-
-  const handleFieldChange = (fieldName: string, value: any, fieldPath: string) => {
+  const handleFieldChange = (fieldName: string, value: any, fieldPath: string[]) => {
     const formCopy = { ...form };
-    // formCopy[fieldName].touched = true;
-    // formCopy[fieldName].value = value;
-    // props.forms.updateForm(props.formName, formCopy);
+    let currentField: { [x: string]: IFormState | IFieldState } = formCopy;
+  
+    for (const path of fieldPath) {
+      currentField = currentField[path] as { [x: string]: IFormState | IFieldState };
+    }
+    currentField.value = value;
+  
     // setForm(formCopy);
-    // Do something with the fieldPath
-    console.log("Field Path:", fieldPath);
   };
-
-  const renderField = (fieldKey: string, field: IFieldState | IFormState, fieldPath: string) => {
-    const currentFieldPath = fieldPath ? `${fieldPath}.${fieldKey}` : fieldKey;
-    console.log("currentFieldPath ", currentFieldPath)
+  const renderField = (fieldKey: string, field: IFieldState | IFormState, fieldPath: string[]) => {
+    const currentFieldPath = [...fieldPath, fieldKey];
+    console.log("currentFieldPath ", currentFieldPath.join("."));
 
     let fieldValue;
     if ('value' in field) {
       fieldValue = field.value;
-    return (
-      <div key={1000000* Math.random()}>
-        <label key={1000000* Math.random()}>{fieldKey}</label>
-        <input
-          key={1000000* Math.random()}
-          type="text"
-          // value={fieldValue}
-          onChange={(e) => handleFieldChange(fieldKey, e.target.value, currentFieldPath)}
-        />
-      </div>
-    );
-  }
-  else{
-    Array.from(Object.keys(field || {})).map(k=> console.log(k , field[k]))
-    return <fieldset key={1000000* Math.random()}>
-        <legend>{fieldKey}</legend>
-      {Object.keys(field || {}).map( k=> renderField(k , field[k], fieldPath)
-      )}
-    </fieldset>
+      return (
+        <div key={1000000 * Math.random()}>
+          <label key={1000000 * Math.random()}>{fieldKey}</label>
+          <input
+            key={1000000 * Math.random()}
+            type="text"
+            // value={fieldValue}
+            onChange={(e) => handleFieldChange(fieldKey, e.target.value, currentFieldPath)}
+          />
+        </div>
+      );
+    } else {
+      Array.from(Object.keys(field || {})).map(k => console.log(k, field[k]));
+      return (
+        <fieldset key={1000000 * Math.random()}>
+          <legend>{fieldKey}</legend>
+          {Object.keys(field || {}).map(k => renderField(k, field[k], currentFieldPath))}
+        </fieldset>
+      );
+    }
+  };
 
-  }
-}
-  const renderForm = (formKey: string, form: IFormState, fieldPath?: string) => {
+  const renderForm = (formKey: string, form: IFormState, fieldPath: string[]) => {
     const fieldKeys = Object.keys(form);
     const fieldsJSX = fieldKeys.map((k) => renderField(k, form[k], fieldPath));
     return (
-      <div>
-        <h1 key={1000000* Math.random()} style={{textAlign: "center"}}>Test form</h1>
-         <fieldset key={1000000* Math.random()}>
-        <legend key={1000000* Math.random()}>{formKey}</legend>
-        {fieldsJSX}
+      <form>
+        <h1 key={1000000 * Math.random()} style={{ textAlign: "center" }}>Test form</h1>
+        <fieldset key={1000000 * Math.random()}>
+          <legend key={1000000 * Math.random()}>{formKey}</legend>
+          {fieldsJSX}
         </fieldset>
-      </div>
+      </form>
     );
   };
 
@@ -70,7 +69,7 @@ export function FormComponent(props: Props) {
     return null;
   }
 
-  return renderForm(props.formName || "", form, "");
+  return renderForm(props.formName || "", form, []);
 }
 
 const formStore = new FormStore();
@@ -78,5 +77,5 @@ const rootForm = formStore.getForm("personalDetails");
 export const RootFormComponent = () => {
   const [form, setForm] = useState(formStore.getForm("personalDetails"));
 
- return  <FormComponent forms={formStore} form={rootForm} path={["forms"]} formName="personalDetails" />
+  return <FormComponent forms={formStore} form={rootForm} path={["forms"]} formName="personalDetails" />
 };
